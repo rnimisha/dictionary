fecbcfd0646d3c8139058332a9c2889247ccf53e
rnimisha <nimisaraut.gmail.com>
2022-09-13 07:09:23 +0545

    audio pronunciation added

--- src/Components/WordHeader/WordHeader.tsx
+++ src/Components/WordHeader/WordHeader.tsx
@@ -12,6 +12,9 @@ type PropType = {
 const WordHeader = ({wordData, word} : PropType) => {
 
     const phonetic = wordData? wordData[0].phonetic ? wordData[0].phonetic : '' : ''
+    const phoentics = wordData? wordData[0].phonetics ? wordData[0].phonetics[0] :  '' : ''
+    const audioUrl = phoentics? phoentics.audio? phoentics.audio.replace('//ssl', 'https://ssl') :' ': ''
+
     return (
         <WordHeaderContainer>
             <Word>
@@ -19,7 +22,10 @@ const WordHeader = ({wordData, word} : PropType) => {
                 <Span>{phonetic}</Span>
             </Word>
             <div>
-                <VolumeIcon/>
+                {audioUrl && <VolumeIcon onClick={()=>{
+                    const audio = new Audio(audioUrl)
+                    audio.play();
+                }}/>}
             </div>
         
         </WordHeaderContainer>
